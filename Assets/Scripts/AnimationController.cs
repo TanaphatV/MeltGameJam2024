4130b1c075beb5a30f6e4e81f1eeef9702db733d
Tanaphat Vivattanakijcharoen <tanaphatviva@gmail.com>
2024-04-16 03:09:04 +0700

    completed player attack

--- Assets/Scripts/AnimationController.cs
+++ Assets/Scripts/AnimationController.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Events;
 public class AnimationController : MonoBehaviour
 {
     [SerializeField] Animator animator;
     [SerializeField] SpriteRenderer spriteRenderer;
     string currentState;
+    public UnityAction onAnimationEnd;
+
     void Start()
     {
         
@@ -18,16 +20,37 @@ public class AnimationController : MonoBehaviour
         
     }
 
-    public void ChangeAnimState(string newState,bool isRight)
-    {
+    bool uninterruptable = false;
 
-            spriteRenderer.flipX = isRight;
+    public void ChangeAnimState(string newState,bool isRight,bool uninterruptable = false)
+    {
+        if (this.uninterruptable)
+            return;
+        spriteRenderer.flipX = isRight;
 
         if (currentState == newState)
             return;
+        if (uninterruptable)
+            StartCoroutine(UninterruptableIE());
 
+        StartCoroutine(AnimationEventIE());
         animator.Play(newState);
         currentState = newState;
+    }
+
+    IEnumerator UninterruptableIE()
+    {
+        this.uninterruptable = true;
+        yield return new WaitForEndOfFrame();
+        yield return new WaitUntil(() => { return animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1; });
+        this.uninterruptable = false;
+    }
+    IEnumerator AnimationEventIE()
+    {
+        yield return new WaitForEndOfFrame();
+        yield return new WaitUntil(() => { return animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1; });
+        if (onAnimationEnd != null)
+            onAnimationEnd();
     }
 
 }

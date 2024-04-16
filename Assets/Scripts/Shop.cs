90f8ccbae8a941dcc706e7a205b2f82da14e34f6
Tanaphat Vivattanakijcharoen <tanaphatviva@gmail.com>
2024-04-16 14:48:33 +0700

    remove null ref

--- Assets/Scripts/Shop.cs
+++ Assets/Scripts/Shop.cs
@@ -8,7 +8,7 @@ public class Shop : InteractableObject
     public List<ShopUpgrade> pickAxe;
     public List<ShopUpgrade> freezer;
     public List<ShopUpgrade> reputation;
-    public List<ShopUpgrade> minigame;;
+    public List<ShopUpgrade> minigame;
 
     protected override void InteractBehavior(PlayerInteract playerInteract)
     {
